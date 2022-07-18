4fd4e92d44d7f8ddaf0be817368e11892ae21876
hhwork <hhwork@naver.com>
2022-07-19 00:07:51 +0900

    no message

--- src/pages/TestState.js
+++ src/pages/TestState.js
@@ -1,16 +1,37 @@
-import {useState} from 'react';
-
+import {useEffect, useState} from 'react';
+import TestState2 from "./TestState2";
 const TestState = () => {
 let [count, setCount] = useState(1);
-
+let [name,setName] =useState("하이");
 // function plusCount(){
 //     setCount(count+1);
 // }
+
+const plusCount = () => {
+    setCount(()=>count+1);
+}
+
+const changeName = () =>{
+    // setName(() => ({
+    //     name :"변경된 이름"
+    // }))
+    setName("변경된이름")
+}
+useEffect(()=>{
+    console.log("변경됨");
+
+})
 return <>
     <div>{count}
-        <button onClick={()=>{setCount(count+1)}}>
+        <button onClick={()=>{plusCount()}}>
         숫자 업
         </button>
+
+        <div>
+            <button onClick={() => {changeName()}}>부모에서 이름변경</button>
+            {name}
+            <TestState2 myName ={name}/>
+        </div>
     </div>
 </>
 }
---
+++ src/pages/TestState2.js
@@ -0,0 +1,25 @@
+import {useState,useEffect} from 'react';
+
+const TestState2 = (props) => {
+let[myName, setMyName] = useState(props.myName);
+
+const changMyname = ()=>{
+    setMyName("하이2");
+}
+
+useEffect(()=>{
+    console.log("여기도 변경");
+    setMyName(props.myName);
+})
+return <>
+    <div>
+      
+        <div>
+            <button onClick={()=>{changMyname()}}>이름변경</button>
+            {myName}
+        </div>
+    </div>
+</>
+}
+
+export default TestState2;
